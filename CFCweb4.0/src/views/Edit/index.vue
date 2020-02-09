63e8936db1ba06200ad7410a7e6f7eac19052f6a
Your Name <912300762@qq.com>
2020-02-10 01:29:06 +0800

    完善好了提醒保存功能，现在直接调用子组件的方法来进行保存

--- CFCweb4.0/src/views/Edit/index.vue
+++ CFCweb4.0/src/views/Edit/index.vue
@@ -3,10 +3,16 @@
     <sticky :class-name='"sub-navbar"'>
       <el-button
         type="success"
+        :disabled="!shouldSave"
         @click="submitForm"
       >提交修改</el-button>
     </sticky>
-    <component :is="currentComponent" :isSave="isSave" :shouldSave="shouldSave"></component>
+    <component
+      ref="edit"
+      :is="currentComponent"
+      :shouldSave="shouldSave"
+      @change="handleChange"
+      @successUpdate="onUpdateSuccess" />
   </div>
 </template>
 <script>
@@ -24,7 +30,6 @@ export default {
   },
   data() {
     return {
-      isSave: false,
       shouldSave: false
     }
   },
@@ -34,19 +39,24 @@ export default {
     }
   },
   methods: {
-    submitForm() {
-      console.log('success')
-      this.isSave = !this.isSave
+    handleChange() {
+      this.shouldSave = true
+    },
+    onUpdateSuccess() {
       this.$notify({
         type: 'success',
         message: '提交修改成功',
         title: '成功'
       })
+    },
+    submitForm() {
+      this.shouldSave = false
+      this.$refs.edit.update()
     }
   },
   beforeRouteLeave (to, from, next) {
     const path = from.path.split('/')[2]
-    if (path === 'edit') {
+    if (path === 'edit' && this.shouldSave) {
       this.$confirm('我们不会保留你在本页的任何改动，确认离开？', '提示', {
         confirmButtonText: '确定离开',
         cancelButtonText: '取消',
@@ -77,7 +87,7 @@ export default {
   top: 0;
   width: 100%;
   text-align: right;
-  padding-right: 20px;
+  padding-right: 60px;
   transition: 600ms ease position;
   background: linear-gradient(90deg, rgba(32, 182, 249, 1) 0%, rgba(32, 182, 249, 1) 0%, rgba(33, 120, 241, 1) 100%, rgba(33, 120, 241, 1) 100%);
 }
