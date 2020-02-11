751c7e131fe455d0af3a4a016dc257ec61c5684d
Your Name <912300762@qq.com>
2020-02-12 03:20:23 +0800

    添加后天编辑our team的视图组件

---
+++ CFCweb4.0/src/views/Edit/components/swiper/Swiper.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="swiper-bar">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page ,index) in pages" :key="index" class="team-wrapper">
+        <member
+          @editMember="handleEditMember"
+          @deleteMember="handleDeleteMember"
+          v-for="(item, index) in page"
+          :key="index"
+          :memberInfo="item"></member>
+      </swiper-slide>
+      <div class="swiper-button-prev" slot="button-prev">
+        <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
+        </svg>
+      </div>
+      <div class="swiper-button-next" slot="button-next">
+        <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
+        </svg>
+      </div>
+    </swiper>
+  </div>
+</template>
+<script>
+import { swiper, swiperSlide } from "vue-awesome-swiper"
+import Member from './components/Member'
+export default {
+  name: "Swiper",
+  components: {
+    swiper,
+    swiperSlide,
+    Member
+  },
+  props: {
+    memberInfo: Array
+  },
+  computed: {
+    pages() {
+      let pages = []
+      this.memberInfo.forEach((item, index) => {
+        const page = Math.floor(index / 3)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  },
+  data() {
+    return {
+      swiperOption: {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
+      },
+    }
+  },
+  methods: {
+    findActiveIndex(name) {
+      const index = this.memberInfo.findIndex(item => {
+        return item.name === name
+      })
+      return index
+    },
+    handleEditMember(name) {
+      this.$emit('editMember', this.findActiveIndex(name))
+    },
+    handleDeleteMember(name) {
+      this.$emit('deleteMember', this.findActiveIndex(name))
+    }
+  }
+}
+</script>
+<style scoped>
+.swiper-bar {
+  position: relative;
+  width: 90%;
+  margin: 0 auto;
+  text-align: center;
+  padding-bottom: 50px;
+}
+.swiper-button-prev,
+.swiper-button-next {
+  top: 40%;
+  border: 1px;
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 50%;
+  box-shadow: 0 1px 1.5px 0 rgba(0,0,0,.12), 0 1px 1px 0 rgba(0,0,0,.24);
+  background-image: none;
+  transition: transform 0.6s,box-shadow 0.6s;
+}
+.swiper-button-prev:hover,
+.swiper-button-next:hover {
+  transform: scale(1.1);
+  box-shadow: 0 10px 10px 0 rgba(0,0,0,.19), 0 6px 3px 0 rgba(0,0,0,.23)
+}
+</style>
---
+++ CFCweb4.0/src/views/Edit/components/swiper/components/Member.vue
@@ -0,0 +1,105 @@
+<template>
+  <div :class="['member', { active: isDelete }]">
+    <div class="img" :style="imgStyle"></div>
+    <div class="info">
+      <h3>{{memberInfo.name}}</h3>
+      <h3>{{memberInfo.designation}}</h3>
+      <p>{{memberInfo.detail}}</p>
+    </div>
+    <div class="edit-tools">
+      <el-button
+        type="primary"
+        size="small"
+        round
+        icon="el-icon-edit"
+        @click="handleEditClick"
+      >编辑</el-button>
+      <el-button
+        type="danger"
+        size="small"
+        round
+        icon="el-icon-delete"
+        @click="handleDeleteClick"
+      >删除</el-button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "member",
+  props: {
+    memberInfo: {
+      type: Object,
+      default() {
+        return {
+          imgUrl: '',
+          name: '期待你的加入',
+          designation: '期待你的加入',
+          detail: '期待你的加入'
+        }
+      }
+    },
+    isDelete: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      imgStyle: {
+        // "background-image": `url(${this.memberInfo.imgUrl})`
+        "background-image": `url(http://localhost:8090/img-1581239175760.jpeg)`
+      }
+    }
+  },
+  methods: {
+    handleEditClick() {
+      this.$emit('editMember', this.memberInfo.name)
+    },
+    handleDeleteClick() {
+      this.$emit('deleteMember', this.memberInfo.name)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.member {
+  position: relative;
+  display: inline-block;
+  width: 33.33%;
+  white-space: normal;
+  vertical-align: top;
+  text-align: center;
+  transition: transform 1s ease, opacity 1s ease, visibility 1s ease;
+  &.active {
+    transform: translateY(-80px);
+    opacity: 0;
+    visibility: hidden;
+  }
+  .img {
+    position: relative;
+    display: inline-block;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    opacity: 1;
+    margin: 0 auto;
+    border-radius: 50%;
+    background-color: #f1f1f1;
+    height: 150px;
+    width: 150px;
+  }
+}
+.info {
+  padding: 2% 5%;
+  h3 {
+    margin-block-start: 0;
+    margin-bottom: 10px;
+  }
+  p {
+    font-size: 18px;
+    line-height: 30px;
+    font-weight: 300;
+  }
+}
+</style>
