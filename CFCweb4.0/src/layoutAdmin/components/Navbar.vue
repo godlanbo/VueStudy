6534fd3f6bae572ebbb2d83fea58622691fec456
Your Name <912300762@qq.com>
2020-02-15 14:39:33 +0800

    添加登出，home和blog的跳转

--- CFCweb4.0/src/layoutAdmin/components/Navbar.vue
+++ CFCweb4.0/src/layoutAdmin/components/Navbar.vue
@@ -16,11 +16,11 @@
               Home
             </el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
-            <el-dropdown-item>Github</el-dropdown-item>
-          </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
+          <!-- <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
+            <el-dropdown-item>Home</el-dropdown-item>
+          </a> -->
+          <a target="_blank" href="https://blog.crazyforcode.org/">
+            <el-dropdown-item>Blog</el-dropdown-item>
           </a>
           <el-dropdown-item divided @click.native="logout">
             <span style="display:block;">Log Out</span>
@@ -51,7 +51,7 @@ export default {
       this.$store.dispatch('app/toggleSideBar')
     },
     async logout() {
-      await this.$store.dispatch('user/logout')
+      await this.$store.dispatch('app/logout')
       this.$router.push(`/login?redirect=${this.$route.fullPath}`)
     }
   }
