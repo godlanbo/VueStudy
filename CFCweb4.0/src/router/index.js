47ff3c7ad0078f7c59c913312d326fea7a3e266e
Your Name <912300762@qq.com>
2020-02-24 01:16:37 +0800

    改为history模式

--- CFCweb4.0/src/router/index.js
+++ CFCweb4.0/src/router/index.js
@@ -72,7 +72,7 @@ const constantRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
