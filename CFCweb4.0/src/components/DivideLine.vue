7545be169387bcec90b11bd84b96c94e11a6e7c7
Your Name <912300762@qq.com>
2020-02-23 18:06:52 +0800

    引用分界线图源改为本地图源，加快加载速度

--- CFCweb4.0/src/components/DivideLine.vue
+++ CFCweb4.0/src/components/DivideLine.vue
@@ -14,18 +14,18 @@ export default {
 </script>
 <style lang="scss" scoped>
 .divide-line.grayTop {
-  background: url(https://balloonary.io/gray_terminator_top.png) repeat-x 50%;
+  background: url(/images/gray_terminator_top.png) repeat-x 50%;
   background-size: 70px;
   height: 35px;
   position: absolute;
-  bottom: 0;
+  bottom: -1px;
   left: 0;
   right: 0;
   margin: 0 auto;
   z-index: 10;
 }
 .divide-line.grayBottom {
-  background: url(https://balloonary.io/gray_terminator_bottom.png) repeat-x 50%;
+  background: url(/images/gray_terminator_bottom.png) repeat-x 50%;
   background-size: 70px;
   height: 35px;
   position: absolute;
