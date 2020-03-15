c7678fd36ea2364ad1ebc8badbb11bb5fb8e2962
Your Name <912300762@qq.com>
2020-03-15 19:03:08 +0800

    按需引入，减小包大小，加快加载速度

--- CFCweb4.0/src/main.js
+++ CFCweb4.0/src/main.js
@@ -2,7 +2,37 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui'
+import {
+  Button,
+  Dialog,
+  Input,
+  Select,
+  Menu,
+  MenuItem,
+  Submenu,
+  MenuItemGroup,
+  Table,
+  TableColumn,
+  DatePicker,
+  Breadcrumb,
+  BreadcrumbItem,
+  Form,
+  FormItem,
+  Icon,
+  Upload,
+  Card,
+  MessageBox,
+  Option,
+  Message,
+  Notification,
+  Timeline,
+  TimelineItem,
+  Divider,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Scrollbar
+} from 'element-ui'
 import VeMap from 'v-charts/lib/map.common'
 import 'element-ui/lib/theme-chalk/index.css'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
@@ -12,12 +42,46 @@ import '@/icons' // icon
 import './permission'
 
 Vue.component(VeMap.name, VeMap)
-Vue.use(ElementUI)
+
+// Vue.use(ElementUI)
+Vue.use(Scrollbar)
+Vue.use(Dropdown);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
+Vue.use(Dialog)
+Vue.use(Input)
+Vue.use(Select)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Option)
+Vue.use(Button);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(DatePicker);
+Vue.use(Breadcrumb);
+Vue.use(BreadcrumbItem);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Icon);
+Vue.use(Upload);
+Vue.use(Card);
+Vue.use(Divider);
+Vue.use(Timeline);
+Vue.use(TimelineItem);
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$notify = Notification;
+Vue.prototype.$message = Message;
+
 Vue.use(VueAwesomeSwiper)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  // mounted: function() {
+  //   document.dispatchEvent(new Event('render-event'));
+  // },
   render: h => h(App),
 }).$mount('#app')
